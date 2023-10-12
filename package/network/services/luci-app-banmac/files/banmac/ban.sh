fbf99dca4c9223e5759ecab10f57a4e78a53ddab
syb999 <13062678135@163.com>
2023-10-12 23:38:06 +0800

    luci-app-banmac: compatible with 23.05

--- package/network/services/luci-app-banmac/files/banmac/ban.sh
+++ package/network/services/luci-app-banmac/files/banmac/ban.sh
@@ -7,12 +7,12 @@ hostname=$(grep -n $getcmac /tmp/dhcp.leases | cut -d ' ' -f 4)
 hostip=$(grep -n $getcmac /tmp/dhcp.leases | cut -d ' ' -f 3)
 echo "★禁网设备：$hostname($hostip) MAC地址：$getcmac 操作日期：$(date +%Y年%m月%d日\ %H点%M分%S秒)" >> /etc/banmaclog
 
-for i in $(seq 0 1)
-do
-	for x in $(iw wlan${i} station dump | grep -i station | cut -d ' ' -f 2)
-	do
-		if [ $x = $getcmac ]; then
-			iw dev wlan${i} station del $x
-		fi
+for i in $(seq 0 1);do
+	for j in $(seq 0 1);do
+		for x in $(iw dev phy${i}-ap${j} station dump | grep -i station | awk '{print$2}');do
+			if [ $x = $getcmac ]; then
+				iw dev phy${i}-ap${j} station del $x
+			fi
+		done
 	done
 done
